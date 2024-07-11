17e607827ba98fc5738039bb0fa19020ac0ea6f7
yyz <3274166958@qq.com>
2024-07-11 17:31:03 +0800

    Add solution for MyObject FPS

--- FPS/FPS.cpp
+++ FPS/FPS.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
-#include "olcConsoleGameEngine.h" 
+#include "olcConsoleGameEngine.h"
 
 class FPSGame : public olcConsoleGameEngine
 {
@@ -10,6 +10,16 @@ public:
     FPSGame() { m_sAppName = L"3D FPS Game"; }
 
 private:
+    struct Enemy
+    {
+        float x, y;
+    };
+
+    struct Bullet
+    {
+        float x, y, angle;
+    };
+
     int nMapWidth = 16;
     int nMapHeight = 16;
     float fPlayerX = 14.7f;
@@ -18,7 +28,8 @@ private:
     float fFOV = 3.14159f / 4.0f;
     float fDepth = 16.0f;
     float fSpeed = 5.0f;
-
+    std::vector<Enemy> enemies;
+    std::vector<Bullet> bullets;
     std::wstring map;
 
 protected:
@@ -40,12 +51,17 @@ protected:
         map += L"#......#########";
         map += L"#..............#";
         map += L"################";
+
+        // Add enemies at fixed positions
+        enemies.push_back({ 8.0f, 8.0f });
+        enemies.push_back({ 10.0f, 10.0f });
+
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime)
     {
-        // 四个按键W、A、S、D控制移动
+        // Handle player movement
         if (GetKey(L'W').bHeld)
         {
             fPlayerX += cosf(fPlayerA) * fSpeed * fElapsedTime;
@@ -74,13 +90,44 @@ protected:
         if (GetKey(L'D').bHeld)
             fPlayerA += (fSpeed * 0.75f) * fElapsedTime;
 
-        // 渲染
+        // Handle shooting
+        if (GetKey(L' ').bPressed)
+        {
+            Bullet b = { fPlayerX, fPlayerY, fPlayerA };
+            bullets.push_back(b);
+        }
+
+        // Update bullets
+        for (auto& b : bullets)
+        {
+            b.x += cosf(b.angle) * fSpeed * fElapsedTime;
+            b.y += sinf(b.angle) * fSpeed * fElapsedTime;
+
+            // Check for collision with walls
+            if (map[(int)b.y * nMapWidth + (int)b.x] == '#')
+            {
+                b.x = -1; // Mark bullet for removal
+            }
+
+            // Check for collision with enemies
+            for (auto& e : enemies)
+            {
+                if (sqrt((b.x - e.x) * (b.x - e.x) + (b.y - e.y) * (b.y - e.y)) < 0.5f)
+                {
+                    e.x = -1; // Mark enemy for removal
+                    b.x = -1; // Mark bullet for removal
+                }
+            }
+        }
+
+        // Remove bullets and enemies that are marked for removal
+        bullets.erase(std::remove_if(bullets.begin(), bullets.end(), [](Bullet& b) { return b.x == -1; }), bullets.end());
+        enemies.erase(std::remove_if(enemies.begin(), enemies.end(), [](Enemy& e) { return e.x == -1; }), enemies.end());
+
+        // Render the scene
         for (int x = 0; x < ScreenWidth(); x++)
         {
-            // 对每一列计算投射到游戏空间的射线角度
             float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)ScreenWidth()) * fFOV;
-
-            // 射线投射的步长，步长越大，精度越低但渲染速度越快
             float fStepSize = 0.1f;
             float fDistanceToWall = 0.0f;
 
@@ -96,7 +143,6 @@ protected:
                 int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
                 int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
 
-                // 测试射线有没有超出边界
                 if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
                 {
                     bHitWall = true;
@@ -104,12 +150,10 @@ protected:
                 }
                 else
                 {
-                    // 射线在边界内，测试射线单元是不是为墙壁
                     if (map[nTestY * nMapWidth + nTestX] == '#')
                     {
                         bHitWall = true;
 
-                        // 突出边界，从每个角落投射射线
                         std::vector<std::pair<float, float>> p;
 
                         for (int tx = 0; tx < 2; tx++)
@@ -122,13 +166,11 @@ protected:
                                 p.push_back(std::make_pair(d, dot));
                             }
 
-                        // 从最近到最远排序
                         std::sort(p.begin(), p.end(), [](const std::pair<float, float>& left, const std::pair<float, float>& right)
                             {
                                 return left.first < right.first;
                             });
 
-                        // 前两/三个最近（看不见四个）
                         float fBound = 0.01f;
                         if (acos(p.at(0).second) < fBound) bBoundary = true;
                         if (acos(p.at(1).second) < fBound) bBoundary = true;
@@ -137,7 +179,6 @@ protected:
                 }
             }
 
-            // 计算到天花板和地板的距离
             int nCeiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)fDistanceToWall);
             int nFloor = ScreenHeight() - nCeiling;
 
@@ -150,7 +191,7 @@ protected:
             else if (fDistanceToWall < fDepth) { nShade = 0x2591; nShadeColor = FG_RED; }
             else { nShade = ' '; nShadeColor = FG_WHITE; }
 
-            if (bBoundary)       nShade = ' ';
+            if (bBoundary) nShade = ' ';
 
             for (int y = 0; y < ScreenHeight(); y++)
             {
@@ -160,7 +201,6 @@ protected:
                     Draw(x, y, nShade, nShadeColor);
                 else
                 {
-                    // 根据距离去渲染地板
                     float b = 1.0f - (((float)y - ScreenHeight() / 2.0f) / ((float)ScreenHeight() / 2.0f));
                     short nFloorShade = ' ';
                     short nFloorColor = FG_WHITE;
@@ -171,6 +211,98 @@ protected:
                     else if (b < 0.9) { nFloorShade = '-'; nFloorColor = FG_GREY; }
                     else { nFloorShade = ' '; nFloorColor = FG_WHITE; }
                     Draw(x, y, nFloorShade, nFloorColor);
+                }
+            }
+        }
+
+        // Render enemies
+        // Render enemies
+        for (auto& e : enemies)
+        {
+            float fVecX = e.x - fPlayerX;
+            float fVecY = e.y - fPlayerY;
+            float fDistanceFromPlayer = sqrt(fVecX * fVecX + fVecY * fVecY);
+
+            float fEyeX = cosf(fPlayerA);
+            float fEyeY = sinf(fPlayerA);
+
+            float fObjectAngle = atan2f(fEyeY, fEyeX) - atan2f(fVecY, fVecX);
+            if (fObjectAngle < -3.14159f)
+                fObjectAngle += 2.0f * 3.14159f;
+            if (fObjectAngle > 3.14159f)
+                fObjectAngle -= 2.0f * 3.14159f;
+
+            bool bInFOV = fabs(fObjectAngle) < fFOV / 2.0f;
+
+            if (bInFOV && fDistanceFromPlayer >= 0.5f && fDistanceFromPlayer < fDepth)
+            {
+                int nObjectCeiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)fDistanceFromPlayer);
+                int nObjectFloor = ScreenHeight() - nObjectCeiling;
+                int nObjectHeight = nObjectFloor - nObjectCeiling;
+                int nObjectAspectRatio = 1;
+                int nObjectWidth = nObjectHeight / nObjectAspectRatio;
+
+                float fObjectMidX = (0.5f * (fObjectAngle / (fFOV / 2.0f)) + 0.5f) * (float)ScreenWidth();
+                for (int lx = 0; lx < nObjectWidth; lx++)
+                {
+                    for (int ly = 0; ly < nObjectHeight; ly++)
+                    {
+                        int nObjX = fObjectMidX + lx - (nObjectWidth / 2);
+                        int nObjY = nObjectCeiling + ly;
+
+                        // Draw human shape
+                        if (lx == nObjectWidth / 2)
+                        {
+                            Draw(nObjX, nObjY, PIXEL_SOLID, FG_RED); // Body
+                        }
+                        else if (ly == nObjectHeight / 5)
+                        {
+                            Draw(nObjX, nObjY, PIXEL_SOLID, FG_RED); // Head
+                        }
+                        else if (lx == nObjectWidth / 4 || lx == 3 * nObjectWidth / 4)
+                        {
+                            Draw(nObjX, nObjY, PIXEL_SOLID, FG_RED); // Arms and legs
+                        }
+                    }
+                }
+            }
+        }
+
+        // Render bullets
+        for (auto& b : bullets)
+        {
+            float fVecX = b.x - fPlayerX;
+            float fVecY = b.y - fPlayerY;
+            float fDistanceFromPlayer = sqrt(fVecX * fVecX + fVecY * fVecY);
+
+            float fEyeX = cosf(fPlayerA);
+            float fEyeY = sinf(fPlayerA);
+
+            float fObjectAngle = atan2f(fEyeY, fEyeX) - atan2f(fVecY, fVecX);
+            if (fObjectAngle < -3.14159f)
+                fObjectAngle += 2.0f * 3.14159f;
+            if (fObjectAngle > 3.14159f)
+                fObjectAngle -= 2.0f * 3.14159f;
+
+            bool bInFOV = fabs(fObjectAngle) < fFOV / 2.0f;
+
+            if (bInFOV && fDistanceFromPlayer >= 0.5f && fDistanceFromPlayer < fDepth)
+            {
+                int nObjectCeiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)fDistanceFromPlayer);
+                int nObjectFloor = ScreenHeight() - nObjectCeiling;
+                int nObjectHeight = nObjectFloor - nObjectCeiling;
+                int nObjectAspectRatio = 1;
+                int nObjectWidth = nObjectHeight / nObjectAspectRatio;
+
+                float fObjectMidX = (0.5f * (fObjectAngle / (fFOV / 2.0f)) + 0.5f) * (float)ScreenWidth();
+                for (int lx = 0; lx < nObjectWidth; lx++)
+                {
+                    for (int ly = 0; ly < nObjectHeight; ly++)
+                    {
+                        int nObjX = fObjectMidX + lx - (nObjectWidth / 2);
+                        int nObjY = nObjectCeiling + ly;
+                        Draw(nObjX, nObjY, '*', FG_YELLOW);
+                    }
                 }
             }
         }
